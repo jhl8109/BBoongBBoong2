62b2082f6b23e5a30eb0066e95fa2f4e16c7229e
제호 이 <jhl81094@gmail.com>
2022-01-08 00:42:22 +0900

    Update server communication

--- Toilet/app/src/main/java/com/example/toilet/RetrofitService.java
+++ Toilet/app/src/main/java/com/example/toilet/RetrofitService.java
@@ -3,9 +3,19 @@ package com.example.toilet;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface RetrofitService {
     @GET("toilet")
-    Call<List<Result>> getToilet();
+    Call<Result> getToilet();
+    @FormUrlEncoded
+    @POST("toilet/add")
+    Call<Result> addToilet(
+            @Field("lat") Double lat,@Field("lnd") Double lnd,@Field("review") Review review
+    );
+
 }
--- Toilet/app/src/main/java/com/example/toilet/ToiletFragment.java
+++ Toilet/app/src/main/java/com/example/toilet/ToiletFragment.java
@@ -10,12 +10,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import net.daum.mf.map.api.CalloutBalloonAdapter;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
@@ -31,7 +36,8 @@ public class ToiletFragment extends Fragment {
     MapPoint centerPoint;
     MapPOIItem currentLocationMarker = new MapPOIItem();
     MapView mapView;
-    int tag;
+    ViewGroup ct;
+
 
     public ToiletFragment() {
         // Required empty public constructor
@@ -43,19 +49,21 @@ public class ToiletFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
+        //new MarkerEventListener();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ct = container;
         View v = inflater.inflate(R.layout.fragment_toilet, container, false);
         mapView = new MapView(requireContext());
         ViewGroup mapViewContainer = v.findViewById(R.id.map_view);
         mapView.zoomIn(true);
+        mapView.setPOIItemEventListener(poiItemEventListener);
+        mapView.setCalloutBalloonAdapter(new CustomCalloutBalloonAdapter());
         mapView.setCurrentLocationEventListener(new MapView.CurrentLocationEventListener() {
             @Override
             public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
@@ -96,27 +104,7 @@ public class ToiletFragment extends Fragment {
                 PERMISSIONS_REQUEST_READ_LOCATION);
         mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
         mapViewContainer.addView(mapView);
-
         makeMarker();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("localhost:3000")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
-        Call<List<Result>> res = retrofitService.getToilet();
-        res.enqueue(new Callback<List<Result>>() {
-            @Override
-            public void onResponse(Call<List<Result>> call, Response<List<Result>> response) {
-                if (response.isSuccessful()) {
-                    Log.e("test",response.body().toString());
-                }
-
-            }
-            @Override
-            public void onFailure(Call<List<Result>> call, Throwable t) {
-
-            }
-        });
         return v;
     }
     public void makeMarker() {
@@ -125,19 +113,81 @@ public class ToiletFragment extends Fragment {
         MapPoint mapPoint =  MapPoint.mapPointWithGeoCoord(37.5514579595, 126.951949155);
         MapPOIItem marker = new MapPOIItem();
         for(int i = 0; i<dataMarkers.size(); i++) {
+            marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+            marker.setCustomImageResourceId(R.drawable.toiletmarker);
+            marker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+            marker.setCustomImageAnchor(0.5f, 1.0f);
             marker.setItemName(dataMarkers.get(i).getIterName());
             marker.setTag(0);
             marker.setMapPoint(mapPoint);
-            marker.setMarkerType(dataMarkers.get(i).getMarkerType());
             mapView.addPOIItem(marker);
         }
 
     }
-    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
-        String place_name = mapPOIItem.getItemName();
-        tag = mapPOIItem.getTag();
-        Toast.makeText(requireContext(), "클릭 감지", Toast.LENGTH_LONG).show();
+//    class CustomCalloutBalloonAdapter implements CalloutBalloonAdapter{
+//        private final View mCalloutBalloon;
+//
+//        public CustomCalloutBalloonAdapter(){
+//            mCalloutBalloon = getLayoutInflater().inflate(R.layout.balloon_layout, null);
+//        }
+//
+//        @Override
+//        public View getCalloutBalloon(MapPOIItem poiItem){
+//            ((TextView) mCalloutBalloon.findViewById(R.id.ball_address)).setText("주소");
+//            ((TextView) mCalloutBalloon.findViewById(R.id.ball_score)).setText("점수");
+//            ((TextView) mCalloutBalloon.findViewById(R.id.ball_review)).setText("리뷰");
+//            return mCalloutBalloon;
+//        }
+//
+//        @Override
+//        public View getPressedCalloutBalloon(MapPOIItem poiItem){
+//            return null;
+//        }
+//    }
+class CustomCalloutBalloonAdapter implements CalloutBalloonAdapter{
+    private final View mCalloutBalloon;
 
+    public CustomCalloutBalloonAdapter(){
+        mCalloutBalloon = getLayoutInflater().inflate(R.layout.custom_callout_balloon, null);
+    }
+
+    @Override
+    public View getCalloutBalloon(MapPOIItem poiItem){
+        ((TextView) mCalloutBalloon.findViewById(R.id.title)).setText("경상남도 양산시 물금읍 신주로 40");
+        ((TextView) mCalloutBalloon.findViewById(R.id.desc)).setText("score");
+        return mCalloutBalloon;
+    }
+
+    @Override
+    public View getPressedCalloutBalloon(MapPOIItem poiItem){
+        return null;
     }
 }
+    private MapView.POIItemEventListener poiItemEventListener = new MapView.POIItemEventListener() {
+        @Override
+        public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+            //sample code 없음
+            Log.e("111111111111111","진입");
+        }
+
+        @Override
+        public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+            Toast.makeText(requireContext(), "Clicked" + mapPOIItem.getItemName() + " Callout Balloon", Toast.LENGTH_SHORT).show();
+            Log.e("2222222222222222","진입");
+        }
+
+        @Override
+        public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+            //sample code 없음
+            Log.e("333333333333","진입");
+        }
+
+        @Override
+        public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+            //sample code 없음
+            Log.i("444444444444444","진입");
+        }
+    };
+}
+
 
